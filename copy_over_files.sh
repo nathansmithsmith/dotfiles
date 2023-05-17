7f956bd991544fc606646a192199cfb5deaa358b
nathansmithsmith <thenathansmithsmith@gmail.com>
2023-05-17 12:41:41 -0600

    More and more gruvbox

---
+++ copy_over_files.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/sh
+
+cp ~/.zshrc ./
+cp -r ~/.oh-my-zsh ./
+cp ~/.p10k.zsh ./
+cp ~/.fehbg ./
+
+# .config files.
+mkdir .config
+cd .config
+cp -r ~/.config/kitty ./
+cp -r ~/.config/ranger ./
+cp -r ~/.config/i3 ./
+cp -r ~/.config/nvim ./
+cp -r ~/.config/dunst ./
+cp -r ~/.config/rofi ./
+
+# i3blocks without a few things
+cp -r ~/.config/i3blocks ./
+rm i3blocks/scripts/reddit/reddit_rss.json
+rm -rf i3blocks/scripts/scarlett_status
+
+cd ..
---
+++ remove_all.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/sh
+
+rm -rf .p10k.zsh .oh-my-zsh .zshrc .fehbg .config
